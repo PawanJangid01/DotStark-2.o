d0a15e3bca425915bdff3cc799b897a0bcf03db8
PawanJangid01 <pawanjangid@dotstark.com>
2026-03-24 11:19:02 +0530

    DotStark 2.o

---
+++ Controllers/FormController.cs
@@ -0,0 +1,121 @@
+﻿using DotStarkWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PublishedCache;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using Umbraco.Extensions;
+namespace DotStarkWeb.Controllers
+{
+    public class FormController : SurfaceController
+    {
+        private readonly IPublishedContentQuery _contentQuery;
+        private readonly ISqliteFormService _formService;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<FormController> _logger;
+        public FormController(
+            ISqliteFormService formService,
+            IConfiguration configuration,
+            IPublishedContentQuery contentQuery,
+              ILogger<FormController> logger,
+            IUmbracoContextAccessor umbracoContextAccessor,
+            IUmbracoDatabaseFactory databaseFactory,
+            ServiceContext serviceContext,
+            AppCaches appCaches,
+            IProfilingLogger profilingLogger,
+            IPublishedUrlProvider publishedUrlProvider)
+            : base(umbracoContextAccessor, databaseFactory, serviceContext, appCaches, profilingLogger, publishedUrlProvider)
+        {
+            _formService = formService;
+            _contentQuery = contentQuery;
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public IActionResult SubmitForm(string name, string email, string company, string message)
+        {
+
+            if (string.IsNullOrWhiteSpace(name) ||
+                string.IsNullOrWhiteSpace(email) ||
+                string.IsNullOrWhiteSpace(company))
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Fill out all required fields."
+                });
+            }
+            //if (!Regex.IsMatch(contactNumber, @"^\d{10}$"))
+            //{
+            //    return Json(new { success = false, message = "Invalid phone number" });
+            //}
+
+
+            var settings = GetEmailSettings();
+            if (settings == null)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Email settings are not configured."
+                });
+            }
+
+            string url = string.Empty;
+
+            var thankYouLink = settings.Value<IEnumerable<Link>>("thankYouPageUrl");
+                url = thankYouLink?.FirstOrDefault()?.Url;
+            
+            
+
+            _formService.SaveFormData(name, email, company, message);
+
+            _formService.SendBrevoTemplateEmail(name, email);
+
+            // Send email
+            _formService.SendBrevoTemplateEmailToAdmin(name, email, company, message);
+
+
+            return Redirect(url);
+
+        }
+
+
+        private IPublishedContent? GetEmailSettings()
+        {
+            if (!UmbracoContextAccessor.TryGetUmbracoContext(out var context))
+                return null;
+
+            return _contentQuery
+          .ContentAtRoot()
+          .FirstOrDefault(x => x.ContentType.Alias == "smtpEmailCredentials");
+        }
+
+        private string ReplaceOrRemove(string template, string placeholder, string label, string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                // Remove the entire <p> block if value is empty
+                return Regex.Replace(
+                    template,
+                    $"<p><strong>{label}:</strong>\\s*{{{{{placeholder}}}}}</p>",
+                    string.Empty,
+                    RegexOptions.IgnoreCase
+                );
+            }
+
+            return template.Replace($"{{{{{placeholder}}}}}", value);
+        }
+
+    }
+}
---
+++ Services/SqliteFormService.cs
@@ -0,0 +1,222 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Mail;
+using System.Text;
+using System.Text.Json;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Extensions;
+
+
+namespace DotStarkWeb.Services
+{
+    public interface ISqliteFormService
+    {
+        void SaveFormData(
+            string name,
+            string email,
+            string company,
+            string message
+        );
+
+        void SendBrevoTemplateEmail(
+           string name, string email
+        );
+        void SendBrevoTemplateEmailToAdmin(
+            string name,
+            string email,
+            string comapny,
+            string message
+        );
+    }
+
+    public class SqliteFormService : ISqliteFormService
+    {
+        private readonly string _connectionString;
+        private readonly IPublishedContentQuery _contentQuery;
+
+        public SqliteFormService(
+        IConfiguration config,
+        IPublishedContentQuery contentQuery,
+        IUmbracoContextAccessor umbracoContextAccessor)
+        {
+            _connectionString = config.GetConnectionString("umbracoDbDSN");
+            _contentQuery = contentQuery;
+        }
+
+        public void SaveFormData(string name, string email, string company, string message)
+        {
+            using var connection = new SqliteConnection(_connectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+
+            command.CommandText = @"
+                INSERT INTO ContactUsForm 
+                (Name, Email, Company, Message, SubmittedAt)
+                VALUES 
+                ($name, $email, $company, $message, $submittedAt)
+            ";
+
+            command.Parameters.AddWithValue("$name", name);
+            command.Parameters.AddWithValue("$email", email);
+            command.Parameters.AddWithValue("$company", company);
+            command.Parameters.AddWithValue("$message", message);
+            command.Parameters.AddWithValue("$submittedAt", DateTime.UtcNow);
+
+            command.ExecuteNonQuery();
+        }
+
+        private IPublishedContent? GetEmailSettings()
+        {
+            return _contentQuery
+        .ContentAtRoot()
+        .FirstOrDefault(x => x.ContentType.Alias == "smtpEmailCredentials");
+        }
+
+
+        public void SendBrevoTemplateEmail(
+        string name,
+        string email
+     )
+        {
+            var settings = GetEmailSettings();
+            if (settings == null) return;
+
+            var apiKey = settings.Value<string>("smtpAPIKey");
+            var userReciverEmail = email;
+            var senderEmail = settings.Value<string>("senderEmail");
+            var fromName = settings.Value<string>("fromName");
+            var fullName = name;
+            var templateId = 1;
+
+            Dictionary<string, object> templateParams = new Dictionary<string, object>
+               {
+                   { "customer", fullName }
+               };
+
+
+
+            var payload = new
+            {
+                to = new[]
+                {
+            new { email = userReciverEmail, name = fullName }
+        },
+                sender = new
+                {
+                    email = senderEmail,
+                    name = fromName
+                },
+                templateId = templateId,
+                @params = templateParams,
+                replyTo = new
+                {
+                    email = email,
+                    name = fullName
+                }
+            };
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("api-key", apiKey);
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+            );
+
+            var json = JsonSerializer.Serialize(payload);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = client.PostAsync(
+                "https://api.brevo.com/v3/smtp/email",
+                content
+            ).Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = response.Content.ReadAsStringAsync();
+
+                // Log error (choose one)
+                Console.WriteLine("Brevo email failed: " + error);
+                // _logger.LogError("Brevo email failed: {Error}", error);
+
+                return;
+            }
+        }
+
+
+        public void SendBrevoTemplateEmailToAdmin(
+                             string name,
+                            string email,
+                            string company,
+                            string message
+                                        )
+        {
+            var settings = GetEmailSettings();
+            if (settings == null) return;
+
+            var apiKey = settings.Value<string>("smtpAPIKey");
+            var adminEmail = settings.Value<string>("adminEmail");
+            var senderEmail = settings.Value<string>("senderEmail");
+            var fromName = settings.Value<string>("fromName");
+            var fullName = name;
+            var templateId = 2;
+
+            Dictionary<string, object> templateParams = new Dictionary<string, object>
+               {
+                     
+                     { "name", name },
+                     { "Email", email },
+                     { "Company", company },
+                     { "Message", message }
+               };
+
+
+            var payload = new
+            {
+                to = new[]
+                {
+            new { email = adminEmail, name = fullName }
+        },
+                sender = new
+                {
+                    email = senderEmail,
+                    name = fromName
+                },
+                templateId = templateId,
+                @params = templateParams,
+                replyTo = new
+                {
+                    email = email,
+                    name = fullName
+                }
+            };
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("api-key", apiKey);
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+            );
+
+            var json = JsonSerializer.Serialize(payload);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = client.PostAsync(
+                "https://api.brevo.com/v3/smtp/email",
+                content
+            ).Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = response.Content.ReadAsStringAsync();
+
+                // Log error (choose one)
+                Console.WriteLine("Brevo email failed: " + error);
+                // _logger.LogError("Brevo email failed: {Error}", error);
+
+                return;
+            }
+        }
+    }
+}
